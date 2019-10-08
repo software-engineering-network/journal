6685e0adb67047170dc397b8837dda7e87413f72
Jarrod Swords <jswords@igsenergy.com>
2019-10-08 12:01:09 -0400

    Add Entity domain base class

---
+++ Sen.Journal.Domain/Entity.cs
@@ -0,0 +1,47 @@
+﻿namespace Sen.Journal.Domain
+{
+    public abstract class Entity
+    {
+        public Id Id { get; }
+
+        protected Entity(Id id)
+        {
+            Id = id;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Entity;
+
+            if (other is null)
+                return false;
+
+            if (ReferenceEquals(this, other))
+                return true;
+
+            var unsetId = new Id(0);
+            if (Id == unsetId || other.Id == unsetId)
+                return false;
+
+            return Id == other.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return (Id != null ? Id.GetHashCode() : 0);
+        }
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            if (left is null && right is null)
+                return true;
+
+            if (left is null || right is null)
+                return false;
+
+            return left.Equals(right);
+        }
+
+        public static bool operator !=(Entity left, Entity right) => !(left == right);
+    }
+}
--- Sen.Journal.Test/UserTest.cs
+++ Sen.Journal.Test/UserTest.cs
@@ -6,22 +6,53 @@ namespace Sen.Journal.Test
 {
     public class UserTest
     {
+        public UserTest()
+        {
+            User = new User(
+                new Id(1),
+                new EmailAddress("john.doe@gmail.com"),
+                new Password("peanutbuttereggdirt"),
+                new Username("JohnDoe")
+            );
+        }
+
+        private static User CreateUserFromPrimitives(
+            ulong id,
+            string emailAddress,
+            string password,
+            string username
+        )
+        {
+            return new User(
+                new Id(id),
+                new EmailAddress(emailAddress),
+                new Password(password),
+                new Username(username)
+            );
+        }
+
+        public User User { get; set; }
+
         [Theory]
         [InlineData(
+            1,
             "john.doe@gmail.com",
             "peanutbuttereggdirt",
             "JohnDoe"
         )]
         public void WhenInstantiatingAUser_WithValidRequiredArgs_ItReturnsAUser(
+            ulong primitiveId,
             string primitiveEmailAddress,
             string primitivePassword,
             string primitiveUsername
         )
         {
+            var id = new Id(primitiveId);
             var emailAddress = new EmailAddress(primitiveEmailAddress);
             var password = new Password(primitivePassword);
             var username = new Username(primitiveUsername);
             var user = new User(
+                id,
                 emailAddress,
                 password,
                 username
@@ -31,6 +62,47 @@ namespace Sen.Journal.Test
             user.EmailAddress.Should().Be(emailAddress);
             user.Password.Should().Be(password);
             user.Username.Should().Be(username);
+        }
+
+
+        [Theory]
+        [InlineData(
+            1,
+            "john.doe@gmail.com",
+            "peanutbuttereggdirt",
+            "JohnDoe"
+        )]
+        public void WhenCheckingForEquality_ItMatchesIdentifierEquality(
+            ulong primitiveId,
+            string primitiveEmailAddress,
+            string primitivePassword,
+            string primitiveUsername
+        )
+        {
+            var user1 = CreateUserFromPrimitives(
+                primitiveId,
+                primitiveEmailAddress,
+                primitivePassword,
+                primitiveUsername
+            );
+            var user2 = CreateUserFromPrimitives(
+                primitiveId,
+                "",
+                "",
+                ""
+            );
+
+            user1.Should().Be(user2);
+            (user1 == user2).Should().BeTrue();
+        }
+
+        [Fact]
+        public void WhenCheckingForEquality_ItMatchesReferenceEquality()
+        {
+            var user1 = User;
+            var user2 = User;
+
+            user1.Should().Be(user2);
         }
     }
 }
