a16928de2fd1a98ea8678f012de2264907d74179
Jarrod Swords <jswords@igsenergy.com>
2019-10-09 09:22:10 -0400

    Move Entity tests to own test class

--- Sen.Journal.Test/UserTest.cs
+++ Sen.Journal.Test/UserTest.cs
@@ -6,33 +6,6 @@ namespace Sen.Journal.Test
 {
     public class UserTest
     {
-        public UserTest()
-        {
-            User = new User(
-                new Id(1),
-                new EmailAddress("john.doe@gmail.com"),
-                new Password("peanutbuttereggdirt"),
-                new Username("JohnDoe")
-            );
-        }
-
-        private static User CreateUserFromPrimitives(
-            ulong id,
-            string emailAddress,
-            string password,
-            string username
-        )
-        {
-            return new User(
-                new Id(id),
-                new EmailAddress(emailAddress),
-                new Password(password),
-                new Username(username)
-            );
-        }
-
-        public User User { get; set; }
-
         [Theory]
         [InlineData(
             1,
@@ -62,48 +35,6 @@ namespace Sen.Journal.Test
             user.EmailAddress.Should().Be(emailAddress);
             user.Password.Should().Be(password);
             user.Username.Should().Be(username);
-        }
-
-
-        [Theory]
-        [InlineData(
-            1,
-            "john.doe@gmail.com",
-            "peanutbuttereggdirt",
-            "JohnDoe"
-        )]
-        public void WhenCheckingForEquality_ItMatchesIdentifierEquality(
-            ulong primitiveId,
-            string primitiveEmailAddress,
-            string primitivePassword,
-            string primitiveUsername
-        )
-        {
-            var user1 = CreateUserFromPrimitives(
-                primitiveId,
-                primitiveEmailAddress,
-                primitivePassword,
-                primitiveUsername
-            );
-            var user2 = CreateUserFromPrimitives(
-                primitiveId,
-                "",
-                "",
-                ""
-            );
-
-            user1.Should().Be(user2);
-            (user1 == user2).Should().BeTrue();
-        }
-
-        [Fact]
-        public void WhenCheckingForEquality_ItMatchesReferenceEquality()
-        {
-            var user1 = User;
-            var user2 = User;
-
-            user1.Should().Be(user2);
-            (user1 == user2).Should().BeTrue();
         }
     }
 }
