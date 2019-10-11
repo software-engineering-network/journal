0d7076833b575e985c7cc9b1a732169cad1a1e9e
Jarrod Swords <jswords@igsenergy.com>
2019-10-11 16:27:07 -0400

    Add JournalService

--- Sen.Journal.Domain/models/journal/Journal.cs
+++ Sen.Journal.Domain/models/journal/Journal.cs
@@ -2,10 +2,12 @@
 {
     public class Journal : Entity
     {
+        public Id PersonId { get; set; }
         public JournalTitle JournalTitle { get; }
 
         public Journal(
             Id id,
+            Id personId,
             JournalTitle journalTitle
         ) : base(id)
         {
---
+++ Sen.Journal.Domain/services/JounalService.cs
@@ -0,0 +1,36 @@
+﻿using Sen.Journal.Domain.storage;
+
+namespace Sen.Journal.Domain.services
+{
+    public interface IJournalService
+    {
+        Journal CreateJournal(CreateJournalArgs args);
+    }
+
+    public class JournalService : IJournalService
+    {
+        private readonly IRepository<Journal> _journalRepository;
+        
+        public JournalService(IRepository<Journal> journalRepository)
+        {
+            _journalRepository = journalRepository;
+        }
+
+        public Journal CreateJournal(CreateJournalArgs args)
+        {
+            var journal = new Journal(
+                new Id(0), 
+                new Id(args.PersonId),
+                new JournalTitle(args.JournalTitle)
+            );
+
+            return _journalRepository.Create(journal);
+        }
+    }
+
+    public class CreateJournalArgs
+    {
+        public ulong PersonId { get; set; }
+        public string JournalTitle { get; set; }
+    }
+}
