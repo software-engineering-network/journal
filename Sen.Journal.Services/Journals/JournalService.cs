242d86d26ab601b274dcf154b91ffc44478ee344
Jarrod Swords <jswords@igsenergy.com>
2019-10-25 09:00:35 -0400

    Break out IJournalService files

---
+++ Sen.Journal.Services/Journals/JournalService.cs
@@ -0,0 +1,37 @@
+﻿using SoftwareEngineeringNetwork.JournalApplication.Domain;
+using SoftwareEngineeringNetwork.JournalApplication.Domain.JournalManagement;
+
+namespace SoftwareEngineeringNetwork.JournalApplication.Services.Journals
+{
+    public class JournalService : IJournalService
+    {
+        #region Fields
+
+        private readonly IJournalRepository _journalRepository;
+
+        #endregion
+
+        #region Construction
+
+        public JournalService(IJournalRepository journalRepository)
+        {
+            _journalRepository = journalRepository;
+        }
+
+        #endregion
+
+        #region IJournalService Members
+
+        public JournalDto Find(ulong userId, string journalTitle)
+        {
+            return _journalRepository
+                .Find(
+                    new UserId(userId),
+                    new JournalTitle(journalTitle)
+                )
+                .ToJournalDto();
+        }
+
+        #endregion
+    }
+}
