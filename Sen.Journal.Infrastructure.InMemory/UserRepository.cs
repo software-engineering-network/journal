61d320e626f4eadafee0633c0a5a54cfae645a9d
Jarrod Swords <jswords@igsenergy.com>
2019-10-25 09:25:35 -0400

    Infrasturcture organization

--- Sen.Journal.Infrastructure.InMemory/UserRepository.cs
+++ Sen.Journal.Infrastructure.InMemory/UserRepository.cs
@@ -112,34 +112,4 @@ namespace SoftwareEngineeringNetwork.JournalApplication.Infrastructure.InMemory
             return ++maxId;
         }
     }
-
-    public static class PersonMapping
-    {
-        public static Person ToPerson(this User user)
-        {
-            return new Person
-            {
-                Id = user.Id?.Value ?? 0,
-                EmailAddress = user.EmailAddress.Value,
-                Name = user.Name.Value,
-                Password = user.Password.Value,
-                RecordName = user.RecordName.Value,
-                Surname = user.Surname.Value,
-                Username = user.Username.Value
-            };
-        }
-
-        public static User ToUser(this Person person)
-        {
-            return new User(
-                new UserId(person.Id), 
-                new EmailAddress(person.EmailAddress),
-                new Name(person.Name),
-                new Password(person.Password),
-                new RecordName(person.RecordName),
-                new Surname(person.Surname),
-                new Username(person.Username)
-            );
-        }
-    }
 }
---
+++ Sen.Journal.Infrastructure.InMemory/models/PersonMapping.cs
@@ -0,0 +1,34 @@
+﻿using SoftwareEngineeringNetwork.JournalApplication.Domain;
+
+namespace SoftwareEngineeringNetwork.JournalApplication.Infrastructure.InMemory
+{
+    public static class PersonMapping
+    {
+        public static Person ToPerson(this User user)
+        {
+            return new Person
+            {
+                Id = user.Id?.Value ?? 0,
+                EmailAddress = user.EmailAddress.Value,
+                Name = user.Name.Value,
+                Password = user.Password.Value,
+                RecordName = user.RecordName.Value,
+                Surname = user.Surname.Value,
+                Username = user.Username.Value
+            };
+        }
+
+        public static User ToUser(this Person person)
+        {
+            return new User(
+                new UserId(person.Id), 
+                new EmailAddress(person.EmailAddress),
+                new Name(person.Name),
+                new Password(person.Password),
+                new RecordName(person.RecordName),
+                new Surname(person.Surname),
+                new Username(person.Username)
+            );
+        }
+    }
+}
