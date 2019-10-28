5305e3b658eeed631a1e64ef864b915484b386e1
Jarrod Swords <jswords@igsenergy.com>
2019-10-28 15:28:02 -0400

    Rename

---
+++ Sen.Journal.Wpf/register-user-dialog/IRegisterUser.cs
@@ -0,0 +1,10 @@
+﻿using SoftwareEngineeringNetwork.JournalApplication.Domain;
+
+namespace SoftwareEngineeringNetwork.JournalApplication.Wpf
+{
+    public interface IRegisterUser
+    {
+        CreateUser CreateCreateUser();
+        void RegisterUser();
+    }
+}
