081d7e237d130d05ba51cbb37ca21b3aca37c7e1
Jarrod Swords <jswords@igsenergy.com>
2019-10-28 11:37:50 -0400

    Remove unnecessary property

--- Sen.Journal.Domain/commands/users/CreateUserValidator.cs
+++ Sen.Journal.Domain/commands/users/CreateUserValidator.cs
@@ -4,19 +4,12 @@ namespace SoftwareEngineeringNetwork.JournalApplication.Domain
 {
     public class CreateUserValidator : AbstractValidator<CreateUser>
     {
-        #region Fields
-
-        private readonly IUserRepository _userRepository;
-
-        #endregion
-
         #region Construction
 
         public CreateUserValidator(IUserRepository userRepository)
         {
-            _userRepository = userRepository;
             RuleFor(x => x.Username)
-                .Must(x => !_userRepository.UsernameExists(x));
+                .Must(x => !userRepository.UsernameExists(x));
         }
 
         #endregion
